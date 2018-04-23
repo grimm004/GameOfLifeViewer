1f8e0cdf1cf80b4a703331017483a89a4d0990e4
grimm004 <grimm004@gmail.com>
2018-04-23 13:32:55 +0100

    .

---
+++ GameOfLifeSim/GameOfLife.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class GameOfLife
+    {
+        private bool[,] world;
+        private bool[,] nextGeneration;
+        private Task processTask;
+        private bool destroyExteriorCells;
+
+        public GameOfLife(int size, bool destroySideCells = true)
+        {
+            destroyExteriorCells = destroySideCells;
+            if (size <= 0) throw new ArgumentOutOfRangeException("Size must be greater than zero");
+            SizeX = SizeY = size;
+            world = new bool[size, size];
+            nextGeneration = new bool[size, size];
+        }
+
+        public GameOfLife(int xSize, int ySize, bool destroySideCells = true)
+        {
+            destroyExteriorCells = destroySideCells;
+            if (xSize <= 0 || ySize <= 0) throw new ArgumentOutOfRangeException("Size must be greater than zero");
+            SizeX = xSize;
+            SizeY = ySize;
+            world = new bool[xSize, ySize];
+            nextGeneration = new bool[SizeX, SizeY];
+        }
+
+        public int SizeX { get; private set; }
+        public int SizeY { get; private set; }
+        public int Generation { get; private set; }
+
+        public Action<bool[,]> NextGenerationCompleted;
+
+        public bool this[int x, int y]
+        {
+            get { return world[x, y]; }
+            set { world[x, y] = value; }
+        }
+
+        public bool ToggleCell(int x, int y)
+        {
+            bool currentValue = this.world[x, y];
+            return world[x, y] = !currentValue;
+        }
+
+        public void Update()
+        {
+            if (processTask != null && processTask.IsCompleted)
+            {
+                // when a generation has completed
+                // now flip the back buffer so we can start processing on the next generation
+                var flip = nextGeneration;
+                nextGeneration = world;
+                world = flip;
+                Generation++;
+
+                // begin the next generation's processing asynchronously
+                processTask = ProcessGeneration();
+
+                NextGenerationCompleted?.Invoke(world);
+            }
+        }
+
+        public void BeginGeneration()
+        {
+            if (processTask == null || (processTask != null && processTask.IsCompleted))
+            {
+                // only begin the generation if the previous process was completed
+                processTask = ProcessGeneration();
+            }
+        }
+
+        public void Wait()
+        {
+            if (processTask != null)
+            {
+                processTask.Wait();
+            }
+        }
+
+        private Task ProcessGeneration()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                Parallel.For(0, SizeX, x =>
+                {
+                    Parallel.For(0, SizeY, y =>
+                    {
+                        int numberOfNeighbors =
+                              IsNeighborAlive(world, SizeX, SizeY, x, y, -1, 0)
+                            + IsNeighborAlive(world, SizeX, SizeY, x, y, -1, 1)
+                            + IsNeighborAlive(world, SizeX, SizeY, x, y, 0, 1)
+                            + IsNeighborAlive(world, SizeX, SizeY, x, y, 1, 1)
+                            + IsNeighborAlive(world, SizeX, SizeY, x, y, 1, 0)
+                            + IsNeighborAlive(world, SizeX, SizeY, x, y, 1, -1)
+                            + IsNeighborAlive(world, SizeX, SizeY, x, y, 0, -1)
+                            + IsNeighborAlive(world, SizeX, SizeY, x, y, -1, -1);
+
+                        bool shouldLive = false;
+                        bool isAlive = world[x, y];
+
+                        if (isAlive && (numberOfNeighbors == 2 || numberOfNeighbors == 3)) shouldLive = true;
+                        else if (!isAlive && numberOfNeighbors == 3) shouldLive = true;
+                        if (destroyExteriorCells && isAlive && (x <= 0 || y <= 0 || x >= SizeX - 1 || y >= SizeY - 1)) shouldLive = false;
+                        nextGeneration[x, y] = shouldLive;
+                    });
+                });
+            });
+        }
+
+        private static int IsNeighborAlive(bool[,] world, int xSize, int ySize, int x, int y, int offsetx, int offsety)
+        {
+            int result = 0;
+
+            int proposedOffsetX = x + offsetx;
+            int proposedOffsetY = y + offsety;
+            bool outOfBounds = proposedOffsetX < 0 || proposedOffsetX >= xSize | proposedOffsetY < 0 || proposedOffsetY >= ySize;
+            if (!outOfBounds)
+                result = world[x + offsetx, y + offsety] ? 1 : 0;
+            return result;
+        }
+    }
+
+    public class GameOfLifeSnapshot
+    {
+        private bool[,] world;
+
+        public GameOfLifeSnapshot(bool[,] currentWorld)
+        {
+            world = currentWorld;
+        }
+
+        public bool[,] World { get { return world; } }
+
+        public bool this[int x, int y]
+        {
+            get { return world[x, y]; }
+            set { world[x, y] = value; }
+        }
+    }
+}
---
+++ GameOfLifeSim/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace GameOfLife
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string configFile = "GameOfLife\\config.xml";
+
+            SettingsManager settingsManager = new SettingsManager(configFile);
+            settingsManager.LoadSettingsFile();
+
+            using (var game = new GameOfLifeEmulator(settingsManager))
+                game.Run();
+            Environment.Exit(0);
+        }
+    }
+#endif
+
+    /// <summary>
+    /// A class that contains some core constants for the game's mechanics
+    /// </summary>
+    public class Constants
+    {
+        // Updates per second to try and change timings for
+        public const int MAXFPS = 60;
+        public const int MAXGPS = 2;
+    }
+
+    public class SettingsManager
+    {
+        private static Settings settings;
+        private readonly string configFile;
+
+        public SettingsManager(string fileName, Settings initialSettings = null)
+        {
+            configFile = fileName;
+            if (initialSettings != null) settings = initialSettings;
+        }
+
+        public Settings LoadedSettings { get { return settings; } set { settings = value; } }
+
+        public void LoadDefaultSettings()
+        {
+            settings = new Settings()
+            {
+                WIDTH = 1600,
+                HEIGHT = 900,
+                startPositionX = 0,
+                startPositionY = 0,
+                targetFps = 60,
+                targetGps = 30,
+                blockSize = 10,
+                cameraSpeed = 5,
+                gameSaveFile = "centinal.cells",
+                loadFormat = GameLoadFormat.random,
+                randomGameWidth = 240,
+                randomGameHeight = 135,
+                randomGameProbability = 5,
+                cellsPadding = 5,
+                destroySideCells = true
+            };
+        }
+
+        public void CreateDefaultSettingsFile()
+        {
+            LoadDefaultSettings();
+            CreateSettingsFile();
+        }
+
+        public void CreateSettingsFile()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(settings.GetType());
+            XmlWriterSettings xmlSettings = new XmlWriterSettings()
+            {
+                Indent = true,
+                IndentChars = "\t"
+            };
+            using (var writer = XmlWriter.Create(configFile, xmlSettings))
+                xmlSerializer.Serialize(writer, settings);
+        }
+
+        public void LoadSettingsFile()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Settings));
+
+            using (var reader = XmlReader.Create(configFile))
+                settings = (Settings)xmlSerializer.Deserialize(reader);
+        }
+    }
+
+    public class Settings
+    {
+        public int targetFps;
+        public int targetGps;
+        public string gameSaveFile;
+        public int blockSize;
+        public int WIDTH;
+        public int HEIGHT;
+        public int startPositionX;
+        public int startPositionY;
+        public int cameraSpeed;
+        public GameLoadFormat loadFormat;
+        public int randomGameWidth;
+        public int randomGameHeight;
+        public int randomGameProbability;
+        public int cellsPadding;
+        public bool destroySideCells;
+    }
+
+    public enum GameLoadFormat
+    {
+        lifegame,
+        lifegamefile,
+        cells,
+        cellsfile,
+        random
+    }
+}
